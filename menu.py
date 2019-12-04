e85cfa4d4aa5af14a35de5b1ab40dd2f3a110040
Harald <harald.holmqvist@yh.nackademin.se>
2019-12-04 23:04:10 +0100

    Create menu.py
    Start menu-file, calling resp. Hero through Class name

---
+++ menu.py
@@ -0,0 +1,21 @@
+def main():
+    print("""\n\
+       WELCOME TO 'Dungeon Run'!
+
+    Start by choosing your hero
+    1. Knight
+    2. Wizard
+    3. Thief
+\n""")
+    choice = input("Enter number for Hero: ")
+    choice_menu =  {'1': Knight,
+                    '2': Wizard,
+                    '3': Thief}
+    if choice not in choice_menu.keys():
+        print("Please choose a hero")
+    else:
+        choice_menu[choice]()
+
+
+if __name__ == "__main__":
+    main()
