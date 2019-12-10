cebf182c96037a8c68831f29fb0ac7e611c987d2
Skazi <zafertk88@gmail.com>
2019-12-10 17:37:39 +0100

    changed attribute from endurance to stamina

--- Heroes.py
+++ Heroes.py
@@ -2,10 +2,10 @@ import random
 
 
 class Heroes:
-    def __init__(self, name, initiative, endurance, attack, agility):
+    def __init__(self, name, initiative, stamina, attack, agility):
         self.name = name
         self.initiative = initiative
-        self.endurance = endurance
+        self.endurance = stamina
         self.attack = attack
         self.agility = agility
 
@@ -13,17 +13,17 @@ class Heroes:
 class Knight(Heroes):
     def __init__(self):
         super().__init__(name=input("What is the name of your hero?"),
-                         initiative=5, endurance=9, attack=6, agility=4)
+                         initiative=5, stamina=9, attack=6, agility=4)
 
 
 class Wizard(Heroes):
     def __init__(self):
         super().__init__(name=input("What is the name of your hero?"),
-                         initiative=6, endurance=4, attack=9, agility=5)
+                         initiative=6, stamina=4, attack=9, agility=5)
 
 
 class Thief(Heroes):
     def __init__(self):
         super().__init__(name=input("What is the name of your hero?"),
-                         initiative=7, endurance=5, attack=5, agility=7)
+                         initiative=7, stamina=5, attack=5, agility=7)
 
