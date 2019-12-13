d35383bf03f4bf0fead7d9c9970235797266b87d
Dmitry Kosov <dmitry.kosov@yh.nackademin.se>
2019-12-13 13:28:59 +0100

    move_function

--- menues.py
+++ menues.py
@@ -1,19 +1,16 @@
 from colorama import *
 import json
-from json import *
 from os import system, name
-
 
 
 
 def check_if_exists(username):
     with open('game.json', 'r+') as f:
-        name=json.load(f)
-        keys=name.keys()
+        name = json.load(f)
+        keys = name.keys()
         if username in keys:
-            print("TRUE")
+            print(f"You have {username}")
         else:
-            print("FALSE")
             add_to_file(username)
 
 
@@ -31,11 +28,29 @@ def add_to_file(username):
 
 
 def add_points(name, points):
-    with open('game.json', "r") as data:
-        json_data = json.load(data)
+    with open('game.json', "r") as f:
+        json_data = json.load(f)
         json_data[name] += points
-    with open('game.json', "w") as data:
-        data.write(dumps(json_data))
+    with open('game.json', "w") as f:
+        json.dump(json_data, f)
+        f.close()
+
+def printLogo():
+    print(Fore.RED +
+        """
+
+@@@@@@@   @@@  @@@  @@@  @@@   @@@@@@@@  @@@@@@@@   @@@@@@   @@@  @@@     @@@@@@@   @@@  @@@  @@@  @@@  
+@@@@@@@@  @@@  @@@  @@@@ @@@  @@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@     @@@@@@@@  @@@  @@@  @@@@ @@@  
+@@!  @@@  @@!  @@@  @@!@!@@@  !@@        @@!       @@!  @@@  @@!@!@@@     @@!  @@@  @@!  @@@  @@!@!@@@  
+@!@  @!@  !@!  @!@  !@!!@!@!  !@!        !@!       !@!  @!@  !@!!@!@!     !@!  @!@  !@!  @!@  !@!!@!@!  
+!@!  !@!  @!@  !@!  @!@ !!@!  !@! @!@!@  @!!!:!    @!@  !@!  @!@ !!@!     @!@!!@!   @!@  !@!  @!@ !!@!  
+!@!  !!!  !@!  !!!  !@!  !!!  !!! !!@!!  !!!!!:    !@!  !!!  !@!  !!!     !!@!@!    !@!  !!!  !@!  !!!  
+!!:  !!!  !!:  !!!  !!:  !!!  :!!   !!:  !!:       !!:  !!!  !!:  !!!     !!: :!!   !!:  !!!  !!:  !!!  
+:!:  !:!  :!:  !:!  :!:  !:!  :!:   !::  :!:       :!:  !:!  :!:  !:!     :!:  !:!  :!:  !:!  :!:  !:!  
+:::: ::   ::::: ::  ::   ::   ::: ::::   :: ::::   ::::: ::   ::   ::     ::   :::  ::::: ::  ::    ::  
+:: :  :    : :  :   ::    :    :: :: :   : :: ::    : :  :    ::    :     :    : :   : :  :   ::    :   
+
+""")
 
 
 def startScreen():
@@ -55,4 +70,25 @@ def clear_screen():
         _ = system('clear')
 
 
-##########################################
+def print_move():
+    print("CHOOSE DIRECTION TO GO")
+    print("North- N")
+    print("West- W")
+    print("East- E")
+    print("South- S")
+    direction = input("--->")
+    return direction.lower()
+
+def move(direction, x, y):
+    if direction == "n":
+        x -= 1
+    elif direction == "w":
+        y -= 1
+    elif direction == "e":
+        y += 1
+    elif direction == "s":
+        x += 1
+    return x, y
+
+
+    
