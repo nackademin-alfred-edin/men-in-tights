1de3e88f9e70ddafe2172cfc91f86e8205027f95
Spindelfred <57704620+nackademin-alfred-edin@users.noreply.github.com>
2019-12-16 10:49:27 +0100

    Add files via upload

--- demo2.py
+++ demo2.py
@@ -1,55 +1,17 @@
 import menu
-import pickhero
-import color
-import map
-import header
+import dungeon_map
 import os
 
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-#header.printLogo()
-def select_map():
-    size = input("""
-                                Select size of the map:
-                                        1. Small (4x4)
-                                        2. Medium (5x5)
-                                        3. Large (8x8)
-
-                                        """)
-    if size == "1":
-        clear()
-        header.printLogo()
-        printtestmap(4)
-    elif size == "2":
-        clear()
-        header.printLogo()
-        printtestmap(5)
-    elif size == "3":
-        clear()
-        header.printLogo()
-        printtestmap(8)
-
-
-
-def printtestmap(x):
-    x = map.Dungeon(x)
-    x.print_dungeon(0,0)
-
 def game():
-    header.printLogo()
+    menu.printLogo()
     menu.startScreen()
-    clear()
-    header.printLogo()
-    pickhero.main_hero()
-    clear()
-    header.printLogo()
-    select_map()
+    menu.clear()
+    menu.printLogo()
+    menu.hero_menu()
+    menu.clear()
+    menu.printLogo()
+    menu.select_map()
     
-temp = 0 
+
 game()
-#print(sys.path)
-#x = map.Dungeon(8)
-#print(x.dungeon)
-#x.print_dungeon(0,1)
