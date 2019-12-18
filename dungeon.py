7d169a3b89fd825f1ffee2f8040865aa2f29fe95
Dmitry Kosov <dmitry.kosov@yh.nackademin.se>
2019-12-18 12:03:42 +0100

    ai_move() with example

--- dungeon.py
+++ dungeon.py
@@ -1,19 +1,34 @@
 from random import randint
 import monster_treasure as mt
 import color as c
-import menues
+import menues as m
+from time import sleep
  
 
 class Dungeon:
-    def __init__(self, map_size, x, y):
+    def __init__(self, map_size):
         self.map_size = map_size
-        self.start_room = self.start_point(x, y)
+        self.start_room = self.start_point()
         self.dungeon = self.generate_map()
         self.generate_exit()
         
 
-    def start_point(self, x, y):
-        return (x, y)
+    def start_point(self):
+        corner = input("""
+                                        Where do you want to start?
+                                            1. North west
+                                            2. North east
+                                            3. South west
+                                            4. South east
+    """)
+        if corner == "1":
+            return (0, 0)
+        elif corner == "2":
+            return (0, self.map_size-1)
+        elif corner == "3":
+            return (self.map_size-1, 0)
+        elif corner == "4":
+            return (self.map_size-1, self.map_size -1)
 
     def generate_exit(self):
         pass
@@ -38,19 +53,23 @@ class Dungeon:
         
 
 #This function print dungeon for user
-    def print_dungeon(self, x, y):
+    def print_dungeon(self, coordinates):
+        x = coordinates[0]
+        y = coordinates[1]
         for i in range(self.map_size):
             for j in range(self.map_size):
                 v = ""
                 if j == self.map_size - 1:
                     v = "\n"
                 if i == x and j == y:
-                    print(c.color.fg.orange, "[O]", end=v)
+                    print("[O]", end=v)
                 else:
-                    print(c.color.fg.purple, self.dungeon[i][j].marker, end=v + c.color.fg.reset)
+                    print(self.dungeon[i][j].marker, end=v)
     
 
-    def move(self, direction, x, y): # Hero can go outside the grid. No exeption
+    def move(self, direction, coordinates): # Hero can go outside the grid. No exception
+        x = coordinates[0]
+        y = coordinates[1]
         if direction.lower() == "n":
             x -= 1
         elif direction.lower() == "w":
@@ -59,8 +78,83 @@ class Dungeon:
             y += 1
         elif direction.lower() == "s":
             x += 1
+        return (x, y)
 
-        return x, y
+
+
+
+    def ai_move(self, coordinates):
+        x = coordinates[0]
+        y = coordinates[1]
+        north = x - 1
+        west = y - 1
+        east = y + 1
+        south = x + 1
+        if x == 0 and y == 0:
+            functions = [east, south]
+            index = randint(0, 1)
+            if index == 1:
+                x = functions[index]
+            else:
+                y = functions[index]
+        elif x == 0 and y < self.map_size-1:
+            functions = [west, east, south]
+            index = randint(0,2)
+            if index == 2:
+                x = functions[index]
+            else:
+                y = functions[index] 
+        elif x == 0 and y == self.map_size-1:
+            functions = [ west, south]
+            index = randint(0,1)
+            if index == 1:
+                x = functions[index]
+            else:
+                y = functions[index]
+        elif x < self.map_size-1 and y == self.map_size-1:
+            functions = [north, west, south]
+            index = randint(0,2)
+            if index == 0 or index == 2:
+                x = functions[index]
+            else:
+                y = functions[index]             
+        elif x == self.map_size-1 and y == self.map_size-1:
+            functions = [north, west]
+            index = randint(0,1)
+            if index == 0:
+                x = functions[index]
+            else:
+                y = functions[index]
+        elif x == self.map_size-1 and y < self.map_size-1:
+            functions = [north, east]
+            index = randint(0,1)
+            if index == 0:
+                x = functions[index]
+            else:
+                y = functions[index]
+        elif x == self.map_size-1 and y == 0:
+            functions = [north, east]
+            index = randint(0,1)
+            if index == 0:
+                x = functions[index]
+            else:
+                y = functions[index]
+        elif x < self.map_size-1 and y == 0:
+            functions = [north, east, south]
+            index = randint(0,2)
+            if index == 0 or index == 2:
+                x = functions[index]
+            else:
+                y = functions[index]
+        else:
+            functions = [north, west, east, south]
+            index = randint(0,3)
+            if index == 0 or index == 3:
+                x = functions[index]
+            else:
+                y = functions[index]
+        return (x, y)
+                
 
 #Object Room selfgenerate monsters and treasures 
 class Room:
@@ -115,3 +209,18 @@ class Room:
                     self.treasure.append(chest)
 
 
+ds = Dungeon(4)
+coordinates = ds.start_room
+ds.print_dungeon(coordinates)
+f = True
+count = 10
+sleep(2)
+while f:
+    m.clear_screen()
+    coordinates = ds.ai_move(coordinates)
+    ds.print_dungeon(coordinates)
+    sleep(1)
+    count -= 1
+    print(count)
+    if count == 0:
+        f = False
