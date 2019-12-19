5eed40cd5185103673a3c01d3c6dbd3bebff8bb2
Majid Boustanifar <majidbst@gmail.com>
2019-12-19 12:11:22 +0100

    Menu

--- die.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-#Casting die function, taking attribute integer value as argument
-def die(attribute):
-    sum = 0
-    for each in range(attribute):
-        sum += random.randint(1, 6)
-    return sum
